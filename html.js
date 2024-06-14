064dcc7fed6f734b26d67413af1c142be65efb0f
Aayushkumar Yadav <140250471+Aayushkrishnaut@users.noreply.github.com>
2024-06-14 13:38:50 +0530

    Add files via upload

---
+++ html.js
@@ -0,0 +1,20 @@
+const URL = 'https://reqres.in/api/users?page=2';
+
+fetch(URL)
+    .then(res => res.json())
+    .then(result => {
+        let output = "";
+        for (let i = 0; i < result.data.length; i++) {
+            const user = result.data[i];
+            output += `<tr>
+                <td>${user.id}</td>
+                <td>${user.email}</td>
+                <td>${user.first_name}</td>
+                <td>${user.last_name}</td>
+                <td><img src="${user.avatar}" alt="${user.first_name} ${user.last_name}"></td>
+            </tr>`;
+        }
+        console.table(result.data);
+        document.getElementById("records").innerHTML = output;
+    })
+    .catch(error => console.error('Error fetching data:', error));
